f186956d1f6d98f3ca0dc56cef30bf3760ac70a4
deruballos <deruballos@gmail.com>
2025-12-30 01:17:15 -0600

    Healthz en path base

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 app.get('/healthz', (req, res) => res.status(200).send('ok'))
 app.use('/devsu/api/users', usersRouter)
 
+
 //evitamos sync/listen en tests
 let server = null
 if (process.env.NODE_ENV !== 'test') {
